1bc6aa19a9b8293f30205216332c8c52f4d5fe9e
Aghogho Monorien <monorienaghogho@gmail.com>
2022-11-28 13:29:33 -0800

    8. new sentence

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-str = [b for (idx, b) in enumerate([a for a in str.split(' ') if a]) if (idx in (0, 5, 6, 12))]
-str = " ".join(str[1:] + [str[0]])
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[107:112] + str[:6]
 print(str)
