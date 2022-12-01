f5ea2f8c8e71da8f5f11e41ae08f3d469639f537
Aghogho Monorien <monorienaghogho@gmail.com>
2022-12-01 09:41:20 -0800

    1. first toolbox

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print("{a} + {b} = {c}".format(b = b, a = a, c = add(a, b)))
+    print("{a} - {b} = {c}".format(a=a, b=b, c=sub(a, b)))
+    print("{a} * {b} = {c}".format(a=a, b=b, c=mul(a, b)))
+    print("{} / {} = {}".format(a, b, int(div(a, b))))
