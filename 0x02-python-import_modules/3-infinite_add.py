e0455cb6bd825bded68fd7ad916861bf7bc70566
Aghogho Monorien <monorienaghogho@gmail.com>
2022-12-01 10:34:30 -0800

    3. infty add

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    from sys import argv
+
+    args = argv[1:]
+    
+    sums: int = 0
+
+    for value in argv[1:]:
+        sums += int(value)
+
+    print(f"{sums:d}")
