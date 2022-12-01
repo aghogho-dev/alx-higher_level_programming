020d8d3e9ff688b5582cd55f588f1fe848b2db15
Aghogho Monorien <monorienaghogho@gmail.com>
2022-12-01 14:15:29 -0800

    6. calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    from calculator_1 import add, sub, mul, div
+    from sys import exit, argv
+
+    signs = {'+': add, '-': sub, '*': mul, '/': div}
+    print(argv)
+
+    if len(argv[1:]) != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    if argv[2] not in signs:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+    c = argv[2]
+    d = signs[c](a, b)
+    print("{} {} {} = {}".format(a, c, b, d))
