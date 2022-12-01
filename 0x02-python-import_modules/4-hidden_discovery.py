f723f871241e10e59b52789fc23f18f84be42b10
Aghogho Monorien <monorienaghogho@gmail.com>
2022-12-01 13:56:50 -0800

    4. hidden value

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,4 +3,5 @@ if __name__ == '__main__':
     import hidden_4
 
     hiddens = [v for v in dir(hidden_4) if not v.startswith('__')]
-    print(*hiddens, sep='\n')
+    for value in hiddens:
+        print("{}".format(value))
