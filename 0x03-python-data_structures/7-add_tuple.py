e7d50473d9c731b8f36b0c8b26d0dcd7f78fc441
Aghogho Monorien <monorienaghogho@gmail.com>
2022-12-05 14:10:16 -0800

    7. tuple addition

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    lena = len(tuple_a)
-    lenb = len(tuple_b)
-    if lena >= 2 and lenb >= 2:
-        a1, a2 = tuple_a[:2]
-        b1, b2 = tuple_b[:2]
-        return (a1 + b1, a2 + b2)
-    elif lena == 1 and lenb == 2:
-        a1 = tuple_a[0]
-        b1, b2 = tuple_b
-        return (a1 + b1, b2)
-    elif lena == 2 and lenb == 1:
-        a1, a2 = tuple_a
-        b1 = tuple_b[0]
-        return (a1 + b1, a2)
-    elif lena == 0 and lenb > 0:
-        return tuple_b
-    elif lena > 0 and lenb == 0:
-        return tuple_a
-    elif lena == 0 and lenb == 0:
-        return (0, 0)
+    if len(tuple_a) < 2:
+        if len(tuple_a) == 0:
+            tuple_a = (0, 0)
+        else:
+            tuple_a = tuple_a + (0,)
+    if len(tuple_b) < 2:
+        if len(tuple_b) == 0:
+            tuple_b = (0, 0)
+        else:
+            tuple_b = tuple_b + (0,)
+
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
