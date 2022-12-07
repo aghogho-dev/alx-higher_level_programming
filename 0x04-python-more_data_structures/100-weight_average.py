6fac21d7ac1e88c0b11d15a073466db49d9332b6
Aghogho Monorien <monorienaghogho@gmail.com>
2022-12-07 15:53:38 -0800

    13. avg wt

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,6 +2,9 @@
 def weight_average(my_list=[]):
     if len(my_list) == 0 or not isinstance(my_list, list):
         return 0
-    products = sum(map(lambda x: x[0] * x[1], my_list))
-    sum_wt = sum(map(lambda x: x[1], my_list))
+    products = 0
+    sum_wt = 0
+    for one in my_list:
+        products += one[0] * one[1]
+        sum_wt += one[1]
     return products / sum_wt
