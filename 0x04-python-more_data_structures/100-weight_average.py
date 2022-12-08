fdff89e2b24da3975a6ea44a0c573d070e7f7c9e
Aghogho Monorien <monorienaghogho@gmail.com>
2022-12-07 20:42:10 -0800

    13. wt avg

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,10 @@
 #!/src/bin/python3
 def weight_average(my_list=[]):
-    if len(my_list) == 0 or not isinstance(my_list, list):
-        return 0
-    products = 0
-    sum_wt = 0
-    for one in my_list:
-        products += one[0] * one[1]
-        sum_wt += one[1]
-    return products / sum_wt
+    if not isinstance(my_list, list) or len(my_list) == 0:
+        return (0)
+    prod = 0
+    wt = 0
+    for ele in my_list:
+        prod += (ele[0] * ele[1])
+        wt += ele[1]
+    return (prod / wt)
