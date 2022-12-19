3580ab5b12be35b3d0020524243fcd406fe30ac3
Aghogho Monorien <monorienaghogho@gmail.com>
2022-12-19 12:13:41 -0800

    4. div list

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    answer = []
+    for idx in range(list_length):
+        try:
+            ans = my_list_1[idx] / my_list_2[idx]
+        except ZeroDivisionError:
+            ans = 0
+            print("division by 0")
+        except IndexError:
+            ans = 0
+            print("out of range")
+        except TypeError:
+            ans = 0
+            print("wrong type")
+        finally:
+            answer.append(ans)
+
+    return answer
+
