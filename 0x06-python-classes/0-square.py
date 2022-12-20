720be2ec0a49f4d667742fa83ed2def7af057670
Aghogho Monorien <monorienaghogho@gmail.com>
2022-12-20 13:25:25 -0800

    0. my first square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""An empty class called Square module
+
+Square is an empty class
+"""
+class Square:
+    """The Square class is an empty class with no attributes
+
+    Attributes:
+        None
+    """
+    pass
