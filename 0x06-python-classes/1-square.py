4c2aaf2eea05a84930343f4e0b6a5b47dcd2bc39
Aghogho Monorien <monorienaghogho@gmail.com>
2022-12-20 13:45:00 -0800

    1. square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A class Called Square."""
+
+
+class Square:
+    """The Square class docstring."""
+
+
+    def __init__(self, size):
+        """Initialization docstring"""
+        self.__size = size
