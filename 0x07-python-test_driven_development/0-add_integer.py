0db38154f244cfc657b51f1d1ffce7c4cac9c9ae
Aghogho Monorien <monorienaghogho@gmail.com>
2023-01-04 11:05:53 -0800

    0-add_integer:

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Addition module."""
+
+def add_integer(a, b=98):
+    """Returns the sum of two ints.
+
+    Raises:
+        TypeError: for types besides int and float
+    """
+    if type(a) not in [int, float]:
+        raise TypeError('a must be an integer')
+    if type(b) not in [int, float]:
+        raise TypeError('b must be an integer')
+    return int(a) + int(b)
