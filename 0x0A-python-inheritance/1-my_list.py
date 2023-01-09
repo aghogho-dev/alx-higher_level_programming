a241405064790338c556cf2ebd376966959230a6
Aghogho <monorienaghogho@gmail.com>
2023-01-09 13:38:18 -0800

    1. MyList

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""MyList module."""
+
+
+class MyList(list):
+    """MyList class."""
+
+    def print_sorted(self):
+        """Print sorted method."""
+        print(sorted(self))
