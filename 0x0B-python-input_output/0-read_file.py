6bff0342159db18d66a3a9b54dd5babdbdada427
Aghogho <monorienaghogho@gmail.com>
2023-01-10 14:31:12 -0800

    0. read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""The read_file.py module."""
+
+
+def read_file(filename=""):
+    """The read_file function.
+
+    Args:
+        filename (str): name of file.
+    """
+    with open(filename, mode='r', encoding='utf-8') as f:
+        data = f.read()
+        print(data)
