3e6ea2c5e0ae2f8db4e3885c52a14334f86053c3
Aghogho <monorienaghogho@gmail.com>
2023-01-10 17:11:56 -0800

    7. add itme

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""The add item module."""
+
+
+def main():
+    """Inside the main func."""
+    import sys
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+    try:
+        obj = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        obj = []
+    
+    obj.extend(sys.argv[1:])
+    save_to_json_file(obj, "add_item.json")
+
+
+if __name__ == '__main__':
+    main()
