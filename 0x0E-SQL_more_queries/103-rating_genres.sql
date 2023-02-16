c852d9558cbc227cc08cbfe7d147f97086a3d7c6
Aghogho <monorienaghogho@gmail.com>
2023-02-15 20:55:06 -0800

    20

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,9 @@
+-- Best genre
+SELECT name, sum(rate) rating
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_show_genres.genre_id = tv_genres.id
+INNER JOIN tv_show ratings
+ON tv_show_ratings.show_id = tv_show_genres.show_id
+GROUP BY 1
+ORDER BY 2 DESC;
