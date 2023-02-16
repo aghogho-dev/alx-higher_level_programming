02d4e1aea40f471ff4d6b9ce958f2678fb4e35dc
Aghogho <monorienaghogho@gmail.com>
2023-02-15 21:01:18 -0800

    20

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -3,7 +3,7 @@ SELECT name, sum(rate) rating
 FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_show_genres.genre_id = tv_genres.id
-INNER JOIN tv_show ratings
+INNER JOIN tv_show_ratings
 ON tv_show_ratings.show_id = tv_show_genres.show_id
 GROUP BY 1
 ORDER BY 2 DESC;
