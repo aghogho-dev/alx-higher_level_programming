8d622efa2262ac495455fae0ce56c24a844f5d5c
Aghogho <monorienaghogho@gmail.com>
2023-03-14 13:26:45 +0000

    fix with semistandard

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const arr = ["C is fun", "Python is cool", "Javascript is amazing"];
-for (ele in arr) {
-	console.log(arr[ele]);
+const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (const ele in arr) {
+  console.log(arr[ele]);
 }
