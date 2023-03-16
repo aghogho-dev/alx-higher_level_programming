4db13ba641e7670fe6515fde5acb890e26d06975
Aghogho <monorienaghogho@gmail.com>
2023-03-16 04:22:09 +0000

    fix semistandard

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const oldDict = require('./101-data').dict;
-let newDict =  {};
-for (let key in oldDict) {
-	if (newDict[oldDict[key]] === undefined) {
-		newDict[oldDict[key]] = [key];
-	} else {
-		newDict[oldDict[key]].push(key);
-	}
+const newDict = {};
+for (const key in oldDict) {
+  if (newDict[oldDict[key]] === undefined) {
+    newDict[oldDict[key]] = [key];
+  } else {
+    newDict[oldDict[key]].push(key);
+  }
 }
 console.log(newDict);
