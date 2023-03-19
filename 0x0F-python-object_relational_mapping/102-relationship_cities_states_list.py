290198187f1e7c093c3c742df7392d9faa6fa7be
Aghogho <monorienaghogho@gmail.com>
2023-03-18 22:09:28 -0700

    17. from city

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""A module called 11-model_state_insert"""
+import sys
+from relationship_state import State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for city in session.query(City).order_by(City.id):
+        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
+
