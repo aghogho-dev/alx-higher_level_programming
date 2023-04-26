a5f0ff2123f92c89e188ed329198bf061ecea4e1
Aghogho <monorienaghogho@gmail.com>
2023-04-26 00:28:10 +0000

    8. right order

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const request = require('request');
+
+const movieId = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+
+request(url, function(error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
+  }
+});
+
+function printCharacters(characters, index) {
+  if (index < characters.length) {
+    request(characters[index], function(error, response, body) {
+      if (!error) {
+        console.log(JSON.parse(body).name);
+        printCharacters(characters, index + 1);
+      }
+    });
+  }
+}
+
