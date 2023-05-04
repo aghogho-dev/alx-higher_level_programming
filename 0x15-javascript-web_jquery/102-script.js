59dd794c05ffc328053973356e0a965d7822614c
aghogho <aghoghomonorien@gmail.com>
2023-05-04 02:19:20 +0100

    102. say hello to everybody

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$('#btn_translate').click(function() {
+		const langCode = $('#language_code').val();
+		const apiUrl = `https://www.fourtonfish.com/hellosalut/hello/${langCode}`;
+		$.get(apiUrl, function(x) {
+			$('#hello').text(x.hello);
+		});
+	});
+});
